7c4a5a6eaedac032753736b807cb9efcd93ab83e
TinoMiguez <cmiguezmartel@gmail.com>
2023-05-29 15:48:44 +0100

    Almost done

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,5 @@
-import React from 'react'
+
 import './App.css'
-import Card from './Components/Card'
 
-function App() {
-
-
-  return (
-    <>
-    <Card/>
-    </>
-  )
-}
 
 export default App
